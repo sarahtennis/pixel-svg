9506cb522651ba55643513b35fc16499be3056b4
Sarah <sarah.n.tennis@gmail.com>
2021-07-28 16:58:52 -0500

    Add color picking functionality, tested updating grid and correct in svg output

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
+    <color-picker :color="color"></color-picker>
     <pixel-grid :grid="grid" :color="color"/>
-    <button @click="generateSvg()">Generate SVG</button>
+    <button @click="generateSvgPaths()">Generate SVG</button>
     <textarea v-model="svg"></textarea>
   </div>
 </template>
 
 <script>
+import ColorPicker from './components/ColorPicker.vue';
 import PixelGrid from './components/PixelGrid.vue';
 import EventBus from './event-bus';
 
 export default {
   name: "App",
   components: {
-    PixelGrid,
+    ColorPicker,
+    PixelGrid
   },
   data: function() {
     return {
@@ -34,8 +37,12 @@ export default {
     EventBus.$on('squareClicked', (position) => {
       this.$set(this.grid[position.x], position.y, this.color);
     });
+    EventBus.$on('colorChanged', (color) => {
+      this.$set(this, 'color', color);
+    })
   },
   methods: {
+    // DEPRECATED
     generateSvg: function() {
       const opening = `<svg xmlns="http://www.w3.org/2000/svg" shape-rendering="crispEdges" style="display: block;" width="${this.grid.length * 10}" height="${this.grid[0].length * 10}" viewBox="0 0 ${(this.grid.length * 10) + ' ' + (this.grid[0].length * 10)}">`;
       let middle = '';
@@ -49,6 +56,33 @@ export default {
         rowIndex ++;
       }
       this.svg = opening + middle + closing;
+    },
+    generateSvgPaths: function() {
+      const opening = `<svg xmlns="http://www.w3.org/2000/svg" width="${this.grid.length * 10}" height="${this.grid[0].length * 10}" viewBox="0 0 ${(this.grid.length * 10) + ' ' + (this.grid[0].length * 10)}">`;
+      const closing = '</svg>';
+      const uniqueColors = {};
+      this.grid.forEach((row, rowIndex) => {
+        row.forEach((square, squareIndex) => {
+          const position = {x: rowIndex, y: squareIndex};
+          if (!uniqueColors[square]) {
+            uniqueColors[square] = [position];
+          } else {
+            uniqueColors[square].push(position)
+          }
+        });
+      });
+      let outputPaths = '';
+      Object.keys(uniqueColors).forEach(colorKey => {
+        const positions = uniqueColors[colorKey];
+        let singlePath = `<path fill="${colorKey}" d="`;
+        positions.forEach(position => {
+          const squarePath = `M ${position.y * 10} ${position.x * 10} h 10 v 10 h -10 L ${position.y * 10} ${position.x * 10} `;
+          singlePath += squarePath;
+        });
+        singlePath += '"/>'
+        outputPaths += singlePath;
+      });
+      this.svg = opening + outputPaths + closing;
     }
   }
 };
---
+++ client/src/components/ColorPicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="color" :value="color" @input="onColorChange($event)">
+</template>
+
+<script>
+import EventBus from '../event-bus';
+
+export default {
+  name: "ColorPicker",
+  props: ['color'],
+  methods: {
+    onColorChange: function(e) {
+      EventBus.$emit('colorChanged', e.target.value);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.pixel-grid {
+  background: black;
+  display: inline-grid;
+  border-top: none;
+  grid-row-gap: 1px;
+  border: 1px solid black;
+}
+</style>
